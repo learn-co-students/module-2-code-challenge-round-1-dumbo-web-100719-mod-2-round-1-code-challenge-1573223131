04e1d11c9f26a467fadb38685c2ab3f600f9d28d
github username <github email address>
2019-11-08 11:07:47 -0500

    done

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,6 +1,27 @@
 class GuestsController < ApplicationController
 
+  before_action :set_guest, only: [:show]
+
   def index
     @guests = Guest.all
   end
+
+  def show
+    @appearances = Appearance.all
+    # @episodes = Episodes.all
+  end
+
+
+  private
+    
+  def set_guest
+    @guest = Guest.find(params[:id])
+  end
+
+  # def guest_params
+  #   params.require(:person).permit(:name, :age)
+  # end
+
+
 end
+
