13851af9441fc5b1cb8a94eacea78d9fbb2deef8
Hope <hopegiometti@gmail.com>
2019-11-08 11:11:43 -0500

    done

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,6 +1,44 @@
 class GuestsController < ApplicationController
-
+  before_action :set_guest, only: [:show, :destroy]
   def index
-    @guests = Guest.all
+    if params[:search]
+      @guests = Guest.search(params[:search])
+    else
+      @guests = Guest.all
+    end
   end
+
+  def show
+  end
+
+  def new
+    @guest = Guest.new
+    @errors = flash[:errors]
+  end
+
+  def create
+    @guest = Guest.create(guest_params)
+
+    if @guest.valid?
+      redirect_to @guest      
+    else
+      flash[:errors] = @guest.errors.full_messages
+      redirect_to new_guest_path
+    end
+  end
+
+  def destroy
+  end
+
+
+  private
+
+  def guest_params
+    params.require(:guest).permit(:name, :occupation, :search)
+  end
+
+  def set_guest
+    @guest = Guest.find(params[:id])
+  end
+
 end
--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,3 +1,11 @@
 class Guest < ApplicationRecord
+    has_many :appearances
+    has_many :episodes, through: :appearances
 
+    validates :name, uniqueness: true
+
+    def self.search(search)
+        where("name LIKE ?", "%#{search}%")
+
+    end
 end
