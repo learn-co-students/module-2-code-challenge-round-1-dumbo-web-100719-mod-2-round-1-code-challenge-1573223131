2091cccca1ca14d876af5e1c42dc7f1efd06090b
Amra Sezairi <amra.sezairi@hotmail.com>
2019-11-08 11:08:09 -0500

    done

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -3,4 +3,25 @@ class GuestsController < ApplicationController
   def index
     @guests = Guest.all
   end
+
+  def show
+    @guest=Guest.find(params[:id])
+  end
+
+  def new 
+    @guest=Guest.new
+    @episode=Episode.new
+  end
+  
+  def create
+    @guest = Guest.create(guest_params)
+    redirect_to episodes_path(@guest.episodes)
+  end
+
+  def guest_params
+    params.require(:guest).permit(:name,:occupation)
+  end
+
+
+
 end
